84b95dceee078ddaaf027bfe4fababa30372461e
Byeonjinha <87685946+Byeonjinha@users.noreply.github.com>
2022-05-31 16:06:34 +0900

    [#1] MapUI 초기

---
+++ PocketCaddy/PocketCaddy/Scene/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  PocketCaddy
+//
+//  Created by Byeon jinha on 2022/05/31.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
