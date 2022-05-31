c4c6debd7958d6739430bcdbae8eb751d116ab9c
Hyeon Sang Lee <250wons@gmail.com>
2022-05-31 22:25:44 +0900

    [#3] Build DescriptionView and test merge

--- PocketCaddy/PocketCaddy/Scene/DescriptionView.swift
+++ PocketCaddy/PocketCaddy/Scene/DescriptionView.swift
@@ -2,16 +2,57 @@
 //  DescriptionView.swift
 //  PocketCaddy
 //
-//  Created by 유정인 on 2022/05/31.
+//  Created by 유정인, 이현상 on 2022/05/31.
 //
 
 import SwiftUI
 
 struct DescriptionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack {
+            // Title and name stack
+            Group {
+                // Title
+                Text("CLUB")
+                    .font(.system(
+                        size: 45,
+                        weight: .heavy,
+                        design: .default)
+                    )
+                // Name of the selected club
+                Text("Driver")
+                    .font(.system(
+                        size: 25,
+                        weight: .medium)
+                    )
+            }
+            .padding(.horizontal, 20)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            // Sample Image
+            Image(systemName: "pencil")
+                .resizable()
+                .frame(width: 300, height: 300)
+            
+            // Two rectangles under the selected club image
+            VStack {
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width: 200, height: 80)
+                
+                Spacer()
+                    .frame(height: 20)
+                
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width: 200, height: 80)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            
+        }// VStack
+    }// body
+}// DescriptionView
 
 struct DescriptionView_Previews: PreviewProvider {
     static var previews: some View {
