9315ad6ec1faac5ba010194ec696ef5d32de280a
김상현 <kshy1019@naver.com>
2022-05-31 15:13:13 +0900

    init xcode project

---
+++ PocketCaddy/PocketCaddy/PocketCaddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketCaddyApp.swift
+//  PocketCaddy
+//
+//  Created by 김상현 on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct PocketCaddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
