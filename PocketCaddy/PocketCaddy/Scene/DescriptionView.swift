cf7f92ae7887cc93681675a79a74d1e6b9a13867
Hyeon Sang Lee <250wons@gmail.com>
2022-06-02 16:20:41 +0900

    [#3] Developed DescriptionUI View

--- PocketCaddy/PocketCaddy/Scene/DescriptionView.swift
+++ PocketCaddy/PocketCaddy/Scene/DescriptionView.swift
@@ -19,8 +19,9 @@ struct DescriptionView: View {
                         weight: .heavy,
                         design: .default)
                     )
+                
                 // Name of the selected club
-                Text("Driver")
+                Text("Cateogry Here")
                     .font(.system(
                         size: 25,
                         weight: .medium)
@@ -33,23 +34,29 @@ struct DescriptionView: View {
             Image(systemName: "pencil")
                 .resizable()
                 .frame(width: 300, height: 300)
+
+            //Club details
+            Title(title: "Name")
+
+            // Horizontal Separator
+            HorizontalSeparator()
             
-            // Two rectangles under the selected club image
-            VStack {
-                Rectangle()
-                    .foregroundColor(.gray)
-                    .frame(width: 200, height: 80)
-                
-                Spacer()
-                    .frame(height: 20)
-                
-                Rectangle()
-                    .foregroundColor(.gray)
-                    .frame(width: 200, height: 80)
+            // Distance, Loft, Length info
+            HStack (spacing: 100) {
+                InfoComponent(title: "Length")
+                InfoComponent(title: "Distance")
+                InfoComponent(title: "Loft")
             }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .trailing)
             
+            HorizontalSeparator()
+            
+            Group {
+                Title(title: "Description")
+                
+                Text("Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World ")
+                    .padding(.horizontal, 15)
+            }
+
         }// VStack
     }// body
 }// DescriptionView
@@ -57,5 +64,39 @@ struct DescriptionView: View {
 struct DescriptionView_Previews: PreviewProvider {
     static var previews: some View {
         DescriptionView()
+    }
+}
+
+struct HorizontalSeparator: View {
+    var body: some View {
+        Rectangle()
+            .frame(width: 350, height: 1)
+            .foregroundColor(.gray)
+            .padding(.vertical, 5)
+    }
+}
+
+struct InfoComponent: View {
+    
+    let title: String
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.system(size: 17, weight: .medium))
+            Text("100")
+        }
+    }
+}
+
+struct Title: View {
+    
+    let title: String
+    
+    var body: some View {
+        Text(title)
+            .padding(.horizontal, 20)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.system(size: 20, weight: .medium))
     }
 }
