8b939d5873f1dc2c295273249576e341edfd65ef
Byeonjinha <87685946+Byeonjinha@users.noreply.github.com>
2022-06-12 16:36:05 +0900

    gif 제거 후 spriteKit으로 action 생성

--- PocketCaddy/PocketCaddy/MainView/LaunchScene.swift
+++ PocketCaddy/PocketCaddy/MainView/LaunchScene.swift
@@ -1,17 +1,50 @@
-//
-//  LaunchScene.swift
-//  PocketCaddy
-//
-//  Created by Byeon jinha on 2022/06/11.
-//
+
 import SpriteKit
-import Foundation
-class GameScene: SKScene, SKPhysicsContactDelegate {
-    static func createFlag() {
+import GameKit
+extension UIImage {
+    static func gradientImage(withBounds: CGRect, startPoint: CGPoint, endPoint: CGPoint , colors: [CGColor]) -> UIImage {
         
-        guard let action = SKEmitterNode(fileNamed: "action") else { return }
-        action.position = CGPoint(x: 0, y: 0)
-//        action.advanceSimulationTime(30)
-//        addChild(action)
+        // Configure the gradient layer based on input
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = withBounds
+        gradientLayer.colors = colors
+        gradientLayer.startPoint = startPoint
+        gradientLayer.endPoint = endPoint
+        // Render the image using the gradient layer
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image!
     }
+}
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var background = SKSpriteNode() // 백그라운드 노드
+    var flag = SKSpriteNode()    // 새 노드
+    // MARK: - Sprites Alignment
+    override func didMove(to view: SKView) {
+        let color1: CGColor = UIColor(red: 44/255, green: 133/255, blue: 45/255, alpha: 1).cgColor
+        let color2: CGColor = UIColor(red: 123/255, green: 183/255, blue: 35/255, alpha: 1).cgColor
+        let startPoint = CGPoint(x: 0, y: 0)
+        let endPoint = CGPoint(x: 1, y: 1)
+        let image: UIImage = UIImage.gradientImage(withBounds: self.frame, startPoint: startPoint, endPoint: endPoint, colors: [color1, color2])
+        let gradientTexture = SKTexture(image: image)
+        let gradientNode = SKSpriteNode(texture: gradientTexture)
+        gradientNode.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        self.addChild(gradientNode)
+        createFlag()
+    }
+    func createFlag() {
+        flag = SKSpriteNode(imageNamed: "flag-1")
+        flag.size = CGSize(width: self.size.width / 8, height:self.size.width / 8)//사이즈
+        flag.position = CGPoint(x: self.size.width / 2, y: self.size.height * 3 / 5)
+        flag.physicsBody?.affectedByGravity = false
+        flag.physicsBody?.isDynamic = false
+        addChild(flag)
+        guard let flyingBySKS = SKAction(named: "flying") else {return  }
+        flag.run(flyingBySKS)
+    }
+    
 }
--- PocketCaddy/PocketCaddy/MainView/LaunchView.swift
+++ PocketCaddy/PocketCaddy/MainView/LaunchView.swift
@@ -8,34 +8,26 @@
 import SwiftUI
 import SpriteKit
 struct LaunchView: View {
-    var bird = SKSpriteNode(imageNamed: "제목_없는_아트워크 6-1(드래그함)")
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
-    let flag = SKAction(named: "action")
-    
+ 
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: screenWidth, height: screenHeight)
+        scene.scaleMode = .fill
+        return scene
+    }
     var body: some View {
         NavigationView{
             NavigationLink(destination: HomeView()){
                 ZStack{
-<<<<<<< Updated upstream
-                    Rectangle()
-                        .foregroundColor(.red)
+                    Rectangle().foregroundColor(.clear)
+                    SpriteView(scene: scene)
+                        .frame(width:screenWidth, height:screenHeight)
                         .ignoresSafeArea()
-                    GifImage("flag")
-                        .frame(width: 610, height: 1000)
-=======
-                    Button(action: {
-                        GameScene.createFlag()
-                    } ){
-                        Rectangle().frame(width: 100, height: 100)
-                    }
-//                    Rectangle()
-//                        .foregroundColor(.red)
-//                        .ignoresSafeArea()
-                   
-//                    GifImage("flag")
-//                        .frame(width: 610, height: 1000)
->>>>>>> Stashed changes
+                    Text("POCKET\nCADDY").font(.system(size: 40))
+                        .foregroundColor(.white)
+                        .bold()
                 }
             }.navigationBarTitleDisplayMode(.inline)
         }
