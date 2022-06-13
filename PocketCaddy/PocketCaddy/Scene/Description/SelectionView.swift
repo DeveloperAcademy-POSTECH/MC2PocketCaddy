9afde65f255d7bf3be861b08a806daf1eb7ba113
Thina <frog7134@gmail.com>
2022-06-14 01:56:46 +0900

    [#25] SampleSelctionView의 코드를 SelectionView와 합치고 디자인에 맞게 수정했습니다.

--- PocketCaddy/PocketCaddy/Scene/Description/SelectionView.swift
+++ PocketCaddy/PocketCaddy/Scene/Description/SelectionView.swift
@@ -10,157 +10,148 @@ import SwiftUI
 struct SelectionView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State var distance: Double = 0
+    @State var currentButtonStatus: Location? = nil
+    
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    let minDistance: Double = 0
+    let maxDistance: Double = 210
     
     var body: some View {
-        VStack {
-            
-            // 전체 뷰
-            HStack {
-                VStack(alignment: .leading) {
-                    // 뒤로가기 버튼
-                    HStack {
-                        Button {
-                            self.presentationMode.wrappedValue.dismiss()
-                        } label: {
-                            Image(systemName: "arrow.left")
-                                .font(Font.system(size: Screen.width * 0.05, weight: .semibold))
-                                .foregroundColor(.primaryGreen)
-                        }
-                        Spacer()
-                    }
-                    
-                    Spacer()
-                    
-                    // 질문
+        let locationButtonArray: [LocationRectangle] = [
+            LocationRectangle(buttonStatus: $currentButtonStatus, buttonName: .fairwayAndRough, buttonImage: "FairwayRough"),
+            LocationRectangle(buttonStatus: $currentButtonStatus, buttonName: .teeingGround, buttonImage: "TeeingGround"),
+            LocationRectangle(buttonStatus: $currentButtonStatus, buttonName: .bunker, buttonImage: "Bunker"),
+            LocationRectangle(buttonStatus: $currentButtonStatus, buttonName: .green, buttonImage: "Green")
+        ]
+        
+        NavigationView {
+            VStack {
+                HStack {
                     VStack(alignment: .leading) {
-                        HStack {
-                            Text("현재")
-                            Text("공")
-                                .foregroundColor(.primaryGreen)
-                            Text("은")
-                                .offset(x: -8)
-                        }
-                        .font(.system(size: UIScreen.main.bounds.width * 0.09, weight: .bold))
-                        
-                        Text("어느 위치에 있나요?")
-                            .font(.system(size: UIScreen.main.bounds.width * 0.09, weight: .bold))
-                            .padding(.bottom, 1)
+                        Text("현재 \(Text("공").foregroundColor(.primaryGreen))은\n어느 위치에 있나요?")
+                            .font(.system(size: Screen.width * 0.09, weight: .bold))
+                            .fixedSize(horizontal: true, vertical: true)
+                            .padding(.bottom, Screen.width * 0.005)
                         
                         Text("공이 위치한 구역과 목표 거리를 입력해주세요 :)")
                             .foregroundColor(.gray)
-                            .font(.system(size: UIScreen.main.bounds.width * 0.038))
+                            .font(.system(size: Screen.width * 0.038))
+                            .fixedSize(horizontal: true, vertical: true)
                     }
-                    .padding(.bottom, UIScreen.main.bounds.height * 0.03)
+                    .padding(.bottom, Screen.height * 0.04)
                     
-                    // Location, Distance
-                    VStack {
-                        HStack {
-                            Text("Location")
-                                .font(Font.system(size: UIScreen.main.bounds.width * 0.07, weight: .bold))
-                            Spacer()
-                            // MapView()로 이동
-                            Button {
-                                
-                            } label: {
-                                Image(systemName: "questionmark.circle")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 20)
-                                    .foregroundColor(.black)
-                            }
-                        }
-                        
-                        // Location 선택 버튼
-                        HStack {
-                            LocationRectangle(buttonName: "Fairway & Rough", buttonImage: "FairwayRough")
-                            LocationRectangle(buttonName: "Teeing Ground", buttonImage: "TeeingGround")
-                        }
-                        HStack {
-                            LocationRectangle(buttonName: "Bunker", buttonImage: "Bunker")
-                            LocationRectangle(buttonName: "Green", buttonImage: "Green")
-                        }
-                        
-                        HStack {
-                            Text("Distance")
-                                .font(Font.system(size: UIScreen.main.bounds.width * 0.07, weight: .bold))
-                            Spacer()
-                        }
-                        .padding(.top, UIScreen.main.bounds.height * 0.03)
-                        
-                        // 남은 거리 텍스트
-                        HStack(spacing: 0) {
-                            Text("“목표 거리는 ")
-                            Text("\(distance, specifier: "%.f")m")
-                                .foregroundColor(.primaryGreen)
-                            Text("입니다.“")
-                        }
-                        .font(Font.system(size: UIScreen.main.bounds.width * 0.045, weight: .bold))
-                        .padding(.top, 5)
-                        
-                        Slider(value: $distance, in: 0...210, step: 5).accentColor(.primaryGreen)
-                        
-                        HStack {
-                            Text("0m")
-                            Spacer()
-                            Text("210m~")
-                        }
-                        .font(Font.system(size: UIScreen.main.bounds.width * 0.042))
-                        
-                        // 선택 버튼
-                        Button {
-                            
-                        } label: {
-                            Text("선택 완료")
-                                .foregroundColor(.white)
-                                .font(Font.system(size: UIScreen.main.bounds.width * 0.045, weight: .bold))
-                                .frame(width: UIScreen.main.bounds.width * 0.3, height: UIScreen.main.bounds.height * 0.065)
-                                .background(
-                                    LinearGradient(gradient: Gradient(colors: [.secondaryGreen, .primaryGreen]), startPoint: .leading, endPoint: .trailing)
-                                        .cornerRadius(UIScreen.main.bounds.height)
-                            )
-                        }
-
+                    Spacer()
+                }
+                
+                // Location, Distance
+                HStack {
+                    Text("Location")
+                        .font(Font.system(size: Screen.width * 0.07, weight: .bold))
+                    Spacer()
+                    // MapView()로 이동
+                    NavigationLink(destination: MapView()) {
+                        Image(systemName: "questionmark.circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20)
+                            .foregroundColor(.black)
                     }
                 }
-                // 전체 뷰에 대한 수평 padding
-                .padding(.horizontal, 10)
+                
+                // Location 선택 버튼
+                LazyVGrid(columns: columns) {
+                    ForEach(0..<locationButtonArray.count, id: \.self) {index in
+                        locationButtonArray[index]
+                            .padding(index % 2 == 0 ? .trailing : .leading, Screen.width * 0.005)
+                    }
+                }
+                
+                HStack {
+                    Text("Distance")
+                        .font(Font.system(size: Screen.width * 0.07, weight: .bold))
+                    Spacer()
+                }
+                .padding(.top, Screen.height * 0.03)
+                
+                Text("“목표 거리는 \(Text("\(Int(distance))m").foregroundColor(.primaryGreen))입니다.“")
+                    .font(Font.system(size: Screen.width * 0.045, weight: .bold))
+                    .padding(.top, 5)
+                
+                Slider(
+                    value: $distance,
+                    in: minDistance...maxDistance,
+                    step: 5
+                ) {
+                    Text("Distance")
+                } minimumValueLabel: {
+                    Text("\(Int(minDistance))m")
+                } maximumValueLabel: {
+                    Text("\(Int(maxDistance))m")
+                }
+                .accentColor(.primaryGreen)
+                
+                NavigationLink(destination: SampleDescriptionView(
+                    locationInfo: currentButtonStatus != nil ? currentButtonStatus! : .fairwayAndRough,
+                    distance: Int(distance)
+                )) {
+                    Text("선택 완료")
+                        .foregroundColor(.white)
+                        .font(Font.system(size: Screen.width * 0.045, weight: .bold))
+                        .frame(width: Screen.width * 0.3, height: Screen.height * 0.06)
+                        .background(
+                            LinearGradient(gradient: Gradient(colors: [.secondaryGreen, .primaryGreen]), startPoint: .leading, endPoint: .trailing)
+                                .cornerRadius(Screen.height)
+                        )
+                }
+                .disabled(currentButtonStatus == nil)
             }
-            .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
+            .frame(maxWidth: Screen.width, maxHeight: Screen.height)
             // 전체 뷰에 대한 padding
-            .padding([.bottom, .horizontal], 60)
+            .padding(.horizontal, Screen.width * 0.06)
+            .padding(.bottom, Screen.width * 0.15)
         }
-        .navigationBarHidden(true)
     }
 }
 
 /// Location 선택 버튼
 struct LocationRectangle: View {
-    @State private var buttonClick: Bool = false
-    var buttonName: String
+    @State var buttonPressed: Bool = false
+    @Binding var buttonStatus: Location?
+    
+    var buttonName: Location
     var buttonImage: String
     
     var body: some View {
         Button {
-            self.buttonClick.toggle()
-            
+            if self.buttonPressed {
+                self.buttonPressed = false
+                buttonStatus = nil
+            } else {
+                self.buttonPressed = true
+                buttonStatus = buttonName
+            }
         } label: {
             ZStack {
                 Image(buttonImage)
                     .resizable()
-                    .frame(width: UIScreen.main.bounds.width * 0.43, height: UIScreen.main.bounds.height * 0.12)
+                    .frame(width: Screen.width * 0.43, height: Screen.height * 0.12)
                     .cornerRadius(8)
-                    .opacity(buttonClick ? 1 : 0.3)
-                Text(buttonName)
-                    .font(Font.system(size: UIScreen.main.bounds.width * 0.042, weight: .bold))
-                    .foregroundColor(buttonClick ? Color.white : Color.black)
+                    .opacity(buttonPressed ? 1 : 0.3)
+                Text(buttonName.rawValue)
+                    .font(Font.system(size: Screen.width * 0.042, weight: .bold))
+                    .foregroundColor(buttonPressed ? Color.white : Color.black)
             }
         }
-
+        .onChange(of: buttonStatus) { newValue in
+            if newValue != buttonName {
+                self.buttonPressed = false
+            }
+        }
     }
 }
 //
-//struct SelectionView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        SelectionView()
-//    }
-//}
+struct SelectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectionView()
+    }
+}
