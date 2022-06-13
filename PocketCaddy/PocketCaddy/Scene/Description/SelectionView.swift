32a3c6fae338f5e532194618627a274d07bb16a4
Thina <frog7134@gmail.com>
2022-06-14 02:02:40 +0900

    [#25] SampleSelctionView의 코드를 SelectionView와 합치고 디자인에 맞게 수정했습니다.

--- PocketCaddy/PocketCaddy/Scene/Description/SelectionView.swift
+++ PocketCaddy/PocketCaddy/Scene/Description/SelectionView.swift
@@ -24,92 +24,90 @@ struct SelectionView: View {
             LocationRectangle(buttonStatus: $currentButtonStatus, buttonName: .green, buttonImage: "Green")
         ]
         
-        NavigationView {
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("현재 \(Text("공").foregroundColor(.primaryGreen))은\n어느 위치에 있나요?")
-                            .font(.system(size: Screen.width * 0.09, weight: .bold))
-                            .fixedSize(horizontal: true, vertical: true)
-                            .padding(.bottom, Screen.width * 0.005)
-                        
-                        Text("공이 위치한 구역과 목표 거리를 입력해주세요 :)")
-                            .foregroundColor(.gray)
-                            .font(.system(size: Screen.width * 0.038))
-                            .fixedSize(horizontal: true, vertical: true)
-                    }
-                    .padding(.bottom, Screen.height * 0.04)
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("현재 \(Text("공").foregroundColor(.primaryGreen))은\n어느 위치에 있나요?")
+                        .font(.system(size: Screen.width * 0.09, weight: .bold))
+                        .fixedSize(horizontal: true, vertical: true)
+                        .padding(.bottom, Screen.width * 0.005)
                     
-                    Spacer()
+                    Text("공이 위치한 구역과 목표 거리를 입력해주세요 :)")
+                        .foregroundColor(.gray)
+                        .font(.system(size: Screen.width * 0.038))
+                        .fixedSize(horizontal: true, vertical: true)
                 }
+                .padding(.bottom, Screen.height * 0.04)
                 
-                // Location, Distance
-                HStack {
-                    Text("Location")
-                        .font(Font.system(size: Screen.width * 0.07, weight: .bold))
-                    Spacer()
-                    // MapView()로 이동
-                    NavigationLink(destination: MapView()) {
-                        Image(systemName: "questionmark.circle")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 20)
-                            .foregroundColor(.black)
-                    }
-                }
-                
-                // Location 선택 버튼
-                LazyVGrid(columns: columns) {
-                    ForEach(0..<locationButtonArray.count, id: \.self) {index in
-                        locationButtonArray[index]
-                            .padding(index % 2 == 0 ? .trailing : .leading, Screen.width * 0.005)
-                    }
-                }
-                
-                HStack {
-                    Text("Distance")
-                        .font(Font.system(size: Screen.width * 0.07, weight: .bold))
-                    Spacer()
-                }
-                .padding(.top, Screen.height * 0.03)
-                
-                Text("“목표 거리는 \(Text("\(Int(distance))m").foregroundColor(.primaryGreen))입니다.“")
-                    .font(Font.system(size: Screen.width * 0.045, weight: .bold))
-                    .padding(.top, 5)
-                
-                Slider(
-                    value: $distance,
-                    in: minDistance...maxDistance,
-                    step: 5
-                ) {
-                    Text("Distance")
-                } minimumValueLabel: {
-                    Text("\(Int(minDistance))m")
-                } maximumValueLabel: {
-                    Text("\(Int(maxDistance))m")
-                }
-                .accentColor(.primaryGreen)
-                
-                NavigationLink(destination: SampleDescriptionView(
-                    locationInfo: currentButtonStatus != nil ? currentButtonStatus! : .fairwayAndRough,
-                    distance: Int(distance)
-                )) {
-                    Text("선택 완료")
-                        .foregroundColor(.white)
-                        .font(Font.system(size: Screen.width * 0.045, weight: .bold))
-                        .frame(width: Screen.width * 0.3, height: Screen.height * 0.06)
-                        .background(
-                            LinearGradient(gradient: Gradient(colors: [.secondaryGreen, .primaryGreen]), startPoint: .leading, endPoint: .trailing)
-                                .cornerRadius(Screen.height)
-                        )
-                }
-                .disabled(currentButtonStatus == nil)
+                Spacer()
             }
-            .frame(maxWidth: Screen.width, maxHeight: Screen.height)
-            // 전체 뷰에 대한 padding
-            .padding(.horizontal, Screen.width * 0.06)
-            .padding(.bottom, Screen.width * 0.15)
+            
+            // Location, Distance
+            HStack {
+                Text("Location")
+                    .font(Font.system(size: Screen.width * 0.07, weight: .bold))
+                Spacer()
+                // MapView()로 이동
+                NavigationLink(destination: MapView()) {
+                    Image(systemName: "questionmark.circle")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20)
+                        .foregroundColor(.black)
+                }
+            }
+            
+            // Location 선택 버튼
+            LazyVGrid(columns: columns) {
+                ForEach(0..<locationButtonArray.count, id: \.self) {index in
+                    locationButtonArray[index]
+                        .padding(index % 2 == 0 ? .trailing : .leading, Screen.width * 0.005)
+                }
+            }
+            
+            HStack {
+                Text("Distance")
+                    .font(Font.system(size: Screen.width * 0.07, weight: .bold))
+                Spacer()
+            }
+            .padding(.top, Screen.height * 0.03)
+            
+            Text("“목표 거리는 \(Text("\(Int(distance))m").foregroundColor(.primaryGreen))입니다.“")
+                .font(Font.system(size: Screen.width * 0.045, weight: .bold))
+                .padding(.top, 5)
+            
+            Slider(
+                value: $distance,
+                in: minDistance...maxDistance,
+                step: 5
+            ) {
+                Text("Distance")
+            } minimumValueLabel: {
+                Text("\(Int(minDistance))m")
+            } maximumValueLabel: {
+                Text("\(Int(maxDistance))m")
+            }
+            .accentColor(.primaryGreen)
+            
+            NavigationLink(destination: SampleDescriptionView(
+                locationInfo: currentButtonStatus != nil ? currentButtonStatus! : .fairwayAndRough,
+                distance: Int(distance)
+            )) {
+                Text("선택 완료")
+                    .foregroundColor(.white)
+                    .font(Font.system(size: Screen.width * 0.045, weight: .bold))
+                    .frame(width: Screen.width * 0.3, height: Screen.height * 0.06)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [.secondaryGreen, .primaryGreen]), startPoint: .leading, endPoint: .trailing)
+                            .cornerRadius(Screen.height)
+                    )
+            }
+            .disabled(currentButtonStatus == nil)
         }
+          .frame(maxWidth: Screen.width, maxHeight: Screen.height)
+          // 전체 뷰에 대한 padding
+          .padding(.horizontal, Screen.width * 0.06)
+          .padding(.bottom, Screen.width * 0.15)
     }
 }
 
