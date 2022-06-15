7cbc43c976a516c61c7baab25dc70212d26c713d
Hyeon Sang Lee <88080251+phainestha1@users.noreply.github.com>
2022-06-15 09:50:22 +0900

    [#50] Reractor: LaunchScene

--- PocketCaddy/PocketCaddy/MainView/LaunchScene.swift
+++ PocketCaddy/PocketCaddy/MainView/LaunchScene.swift
@@ -1,27 +1,29 @@
 import SpriteKit
 
 final class LaunchScene: SKScene, SKPhysicsContactDelegate {
-    
     var background = SKSpriteNode() // 백그라운드 노드
     var flag = SKSpriteNode()    // 새 노드
+    
     // MARK: - Sprites Alignment
     override func didMove(to view: SKView) {
-        
-        
         let color1: CGColor = UIColor(.secondaryGreen).cgColor
         let color2: CGColor = UIColor(.primaryGreen).cgColor
         let startPoint = CGPoint(x: 0, y: 0)
         let endPoint = CGPoint(x: 1, y: 1)
-        guard let image = UIImage.gradientImage(withBounds: self.frame, startPoint: startPoint, endPoint: endPoint, colors: [color1, color2]) else {
+        
+        guard let image = UIImage.gradientImage(withBounds: self.frame, startPoint: startPoint, endPoint: endPoint, colors: [color1, color2])
+        else {
             return
         }
+        
         let gradientTexture = SKTexture(image: image)
         let gradientNode = SKSpriteNode(texture: gradientTexture)
-        gradientNode.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        self.addChild(gradientNode)
-        createFlag()
         
-        //         if let or guard let
+        gradientNode.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        
+        self.addChild(gradientNode)
+        
+        createFlag()
     }
     
     func createFlag() {
@@ -30,9 +32,13 @@ final class LaunchScene: SKScene, SKPhysicsContactDelegate {
         flag.position = CGPoint(x: self.size.width * 13 / 25, y: self.size.height * 13 / 25)
         flag.physicsBody?.affectedByGravity = false
         flag.physicsBody?.isDynamic = false
+        
         addChild(flag)
-        guard let flags = SKAction(named: "flying") else { return }
+        
+        guard let flags = SKAction(named: "flying")
+        else { return }
+        
         flag.run(flags)
     }
     
-}
+}// LaunchScene
