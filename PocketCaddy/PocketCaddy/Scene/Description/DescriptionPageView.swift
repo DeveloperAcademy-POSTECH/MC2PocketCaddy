050abe7a8f7e691872cf24c9ea8cacafccdd8b8d
jaehoon9186 <83233720+jaehoon9186@users.noreply.github.com>
2022-06-18 17:49:30 +0900

    [#86] Fix  DescriptionView WIP

--- PocketCaddy/PocketCaddy/Scene/Description/DescriptionPageView.swift
+++ PocketCaddy/PocketCaddy/Scene/Description/DescriptionPageView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct DescriptionPageView: View {
-    @EnvironmentObject var clubDataManager : ClubDataManager
+    //@EnvironmentObject var clubDataManager : ClubDataManager
     @State var currentPage: Int = 0
     @Binding var goBack: Bool
-    
+    @Binding var selectedClub: [ClubModel]
     @GestureState private var dragOffsetDescriptionPageView = CGSize.zero
 
     var body: some View {
@@ -23,7 +23,7 @@ struct DescriptionPageView: View {
                 CustomBackButtonGoBack(isViewActive: $goBack)
 
             TabView() {
-                ForEach(clubDataManager.selectedClub, id: \.?.name) { clubModel in
+                ForEach(self.selectedClub, id: \.?.name) { clubModel in
                     DescriptionSingleView(selectedClub: clubModel, isViewActive: $goBack)
 
                     } //Foreach
